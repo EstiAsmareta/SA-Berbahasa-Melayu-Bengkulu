c82c218005d7c8a8c3faa7f8d5bf9140c8749537
unknown <asmaretaayu@gmail.com>
2025-10-02 20:01:46 +0700

    manual book

--- home.py
+++ home.py
@@ -28,13 +28,13 @@ st.subheader("Pilih Topik untuk Analisis Sentimen")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    if st.button("Cagub 2025 Provinsi Bengkulu Paslon 01", key="paslon_01"):
-        st.switch_page("pages/paslon_01_app.py") 
+    if st.button("Cagub 2025 Provinsi Bengkulu Paslon 01"):
+        st.switch_page("pages/app.py")
 
 with col2:
-    if st.button("Cagub 2025 Provinsi Bengkulu Paslon 02", key="paslon_02"):
-        st.switch_page("pages/paslon_02_app.py") 
+    if st.button("Cagub 2025 Provinsi Bengkulu Paslon 02"):
+        st.switch_page("pages/app1.py")
 
 with col3:
-    if st.button("Perubahan Julukan Provinsi Bengkulu", key="julukan"):
-        st.switch_page("pages/perubahan_julukan.py")
+    if st.button("Perubahan Julukan Provinsi Bengkulu"):
+        st.switch_page("pages/page2.py")
