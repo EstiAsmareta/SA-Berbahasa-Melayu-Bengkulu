3bfb0496082b0a9c50f2349d94c1fae975969f5b
unknown <asmaretaayu@gmail.com>
2025-10-02 19:56:46 +0700

    manual book

--- home.py
+++ home.py
@@ -1,27 +1,40 @@
 import streamlit as st
 
-st.set_page_config(page_title="Analisis Sentimen Berbahasa Melayu Bengkulu")
+# Mengatur konfigurasi halaman
+st.set_page_config(
+    page_title="Analisis Sentimen Berbahasa Melayu Bengkulu",
+    layout="centered" # Opsional: mengatur layout menjadi terpusat
+)
+
+# --- Tombol Navigasi Manual Book (di atas) ---
+# st.switch_page digunakan untuk navigasi antar halaman di Streamlit
+if st.button("📚 Manual Book / Tentang Aplikasi", type="secondary"):
+    st.switch_page("manual.py") 
+    
+st.markdown("---")
+
 
 # Menambahkan judul ke website
 st.title('Analisis Sentimen Pada Dataset Komentar Berbahasa Melayu Bengkulu')
 
-# Menambahkan teks
+# Menambahkan teks sambutan
 st.write('Halo sanak segalo, kini kito pacak melakukan analisis sentimen menggunokan bahasa Melayu Bengkulu dengan lebih mudah disiko. Mulailah pilih topiknyo dibawah iko!')
 
 # Menambahkan tombol untuk navigasi
-st.write("---")
+st.markdown("---")
 st.subheader("Pilih Topik untuk Analisis Sentimen")
 
+# Menggunakan kolom untuk tata letak tombol
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    if st.button("Cagub 2025 Provinsi Bengkulu Paslon 01"):
-        st.switch_page("pages/app.py")
+    if st.button("Cagub 2025 Provinsi Bengkulu Paslon 01", key="paslon_01"):
+        st.switch_page("pages/paslon_01_app.py") 
 
 with col2:
-    if st.button("Cagub 2025 Provinsi Bengkulu Paslon 02"):
-        st.switch_page("pages/app1.py")
+    if st.button("Cagub 2025 Provinsi Bengkulu Paslon 02", key="paslon_02"):
+        st.switch_page("pages/paslon_02_app.py") 
 
 with col3:
-    if st.button("Perubahan Julukan Provinsi Bengkulu"):
-        st.switch_page("pages/page2.py")
+    if st.button("Perubahan Julukan Provinsi Bengkulu", key="julukan"):
+        st.switch_page("pages/perubahan_julukan.py")
